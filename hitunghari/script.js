909e929f0dbb71b59f41ea58bfc1090f3354fdee
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2021-12-18 11:45:42 +0700

    Update

--- hitunghari/script.js
+++ hitunghari/script.js
@@ -1,15 +1,15 @@
-function hitung(y1, y2) {
-  alert(y1 + "\n" + y2)
-  y1 = new Date(y1).getTime();
-  y2 = new Date(y2).getTime();
-  alert(y1 + "\n" + y2)
-
-  let o = y2 - y1;
-  let h = o / (1000 * 3600 * 24);
-  alert(h)
-  let b = o / (1000 * 3600 * 24 * 30);
-  alert(b)
+function addZero(n) {
+  return (n * 1) < 10 ? "0" + n : n;
 }
 
-alert("Tes")
+function hitung(y1, y2, res) {
+  y1 = new Date(y1).getTime();
+  y2 = new Date(y2).getTime();
 
+  let o = y2 - y1;
+  let h = Math.floor((o / (1000 * 3600 * 24)) % 30);
+  let b = Math.floor((o / (1000 * 3600 * 24 * 30))) % 12;
+  let t = Math.floor((o / (1000 * 3600 * 24 * 30 * 12)));
+
+  res.innerHTML = `${addZero(t)} Tahun<br>${addZero(b)} Bulan<br>${addZero(h)} Hari`
+}
