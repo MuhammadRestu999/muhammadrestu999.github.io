3878b8785dcf37a1d562722406a83113ebb07c03
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2021-12-25 21:00:21 +0700

    Update

--- covid-19/covid.js
+++ covid-19/covid.js
@@ -20,17 +20,15 @@ window.onerror = function (msg, url, lineNo, columnNo, error) {
 
 async function get(a, b, c, d) {
   alert("Tunggu sebentar...")
-  let { data } = await axios("https://hardianto-chan.herokuapp.com/api/info/covid?apikey=hardianto")
-  let idn = data[0].indo
+  let { data } = await axios("https://restu-restapi.herokuapp.com/covid?country=indonesia")
+  let result = data.result
 
-  let { positif_indo, sembuh_indo, meninggal_indo, update_indo } = idn
+  let { positif, sembuh, meninggal, diperbarui } = result
 
-  let tgl = update_indo.split("-")
-  update_indo = `${tgl[2]}-${tgl[1]}-${tgl[0]}`
   alert("Done!")
-  a.innerHTML = positif_indo
-  b.innerHTML = sembuh_indo
-  c.innerHTML = meninggal_indo
-  d.innerHTML = update_indo
+  a.innerHTML = positif
+  b.innerHTML = sembuh
+  c.innerHTML = meninggal
+  d.innerHTML = diperbarui
 
 }
