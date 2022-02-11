f9da76824a44ab9f3aa164fbf38fbe3e38fb97ad
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-02-11 08:39:34 +0700

    Update

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ function loadNav() {
   let success
   let errMes = null
   try {
-    let nav = `      <a href="/">Home</a> | <a href="/about/">About me</a> | <a href="/portal/">Portal Nether</a> | <a href="/umur/">Hitung umur</a> | <a href="/randmath/">Random Math</a> | <a href="/passgen/">Password Generator</a> | <a href="/isi-voucher/">Isi Voucher</a> | <a href="/lirik/">Lirik Lagu</a> | <a href="/covid-19/">Covid-19</a> | <a href="/hitunghari/">Hitung Hari Berlalu</a> | <a href="/doa/">Doa-doa</a>`
+    let nav = `      <a href="/">Home</a> | <a href="/about/">About me</a> | <a href="/portal/">Portal Nether</a> | <a href="/umur/">Hitung umur</a> | <a href="/randmath/">Random Math</a> | <a href="/passgen/">Password Generator</a> | <a href="/isi-voucher/">Isi Voucher</a> | <a href="/lirik/">Lirik Lagu</a> | <a href="/covid-19/">Covid-19</a> | <a href="/hitunghari/">Hitung Hari Berlalu</a>`
     let loc = location.pathname
     let now = nav.substr(nav.indexOf(loc) - 9).split(">")
     now[0] = now[0] + " class=\"saiki\""
