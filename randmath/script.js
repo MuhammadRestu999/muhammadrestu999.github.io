ef85d97886e8e915d637f3c09aa53f41afd482f9
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-03-25 19:47:12 +0700

    Gabut

--- randmath/script.js
+++ randmath/script.js
@@ -28,18 +28,18 @@ function math(diff) {
 let diff = +prompt("Pilih tingkat kesulitan :\n1. Mudah\n2. Normal\n3. Susah\n4. Sangat susah\n5. Benar² susah\n\n0 == Keluar", "")
 
 if(diff == 1) {
-  math(10)
+  math(5)
 } else if(diff == 2) {
-  math(20)
+  math(10)
 } else if(diff == 3) {
-  math(30)
+  math(25)
 } else if(diff == 4) {
-  math(40)
-} else if(diff == 5) {
   math(50)
+} else if(diff == 5) {
+  math(100)
 } else if(diff == 0) {
   alert("Selamat tinggal!")
-  location.href = "/"
+  window.history.back()
 } else {
   alert("Tidak tersedia!")
   location.reload()
